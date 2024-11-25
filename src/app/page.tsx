88262beda20f618234421346ed2c32be223bba76
MbBrainz <mb.brains@gmail.com>
2024-11-25 09:42:04 +0700

    feat: skip widget added to reactlivescope -> error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+"use client";
 import Image from "next/image";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { Widget } from "@skip-go/widget";
+const code = `
+  <div className="justify-center items-center h-[640px] w-[450px]">
+    <Widget />
+  </div>
+`;
 
 export default function Home() {
   return (
@@ -12,16 +20,13 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+
+
+        <LiveProvider code={code} scope={{ Widget }} noInline>
+          <LivePreview />
+          <LiveError />
+          <LiveEditor />
+        </LiveProvider>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
